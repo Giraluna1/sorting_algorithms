561bb371797656e8d70689f05db5cd4813502ce5
Giraluna1 <lunagolo@hotmail.com>
2021-02-02 10:14:52 -0500

    Add bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorts an array in ascending order using Bubble sort algorithm
+* @array: array of integers
+* @size: size of the array
+* Return: 0
+*/
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped = true;
+	size_t i;
+	int temp;
+
+	if (!array || (size == 0))
+		return;
+	if  (size == 1)
+		print_array(array, size);
+
+	while (swapped == true)
+	{
+		swapped = false;
+		for (i = 0; i < size - 1; i++)
+		{
+
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+	}
+}
